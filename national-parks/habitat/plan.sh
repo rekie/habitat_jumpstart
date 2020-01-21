4be97d22b994afbe38571d96cd53b02656e0ab2c
ChefDK User <chef@ip-10-0-1-197.us-west-2.compute.internal>
2020-01-21 12:39:05 +0000

    Changes for simple bind

--- national-parks/habitat/plan.sh
+++ national-parks/habitat/plan.sh
@@ -3,9 +3,12 @@ pkg_origin=rekie
 pkg_version="6.3.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
-pkg_deps=(core/tomcat8 core/jre8)
+pkg_deps=(core/tomcat8 core/jre8 core/mongo-tools)
 pkg_build_deps=(core/jdk8/8.192.0 core/maven)
 pkg_svc_user="root"
+pkg_binds=(
+	[database]="port"
+)
 do_prepare(){ export JAVA_HOME=$(hab pkg path core/jdk8) ; }
 do_build()
 {
@@ -18,4 +21,6 @@ do_install()
     mkdir ${PREFIX}/config
     cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/target/${pkg_name}.war ${PREFIX}/
     cp $(hab pkg path core/tomcat8)/config/conf_server.xml ${PREFIX}/config/
+    cp -v ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/data/national-parks.json ${PREFIX}/
+	
 }
